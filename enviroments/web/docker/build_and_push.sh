c40839caa2c95230e03dd2dda1ad0e610f5dea9d
HarukiKaneko <niku11298@gmail.com>
2022-09-01 22:49:19 +0900

    enviroments: web docker add arg

--- enviroments/web/docker/build_and_push.sh
+++ enviroments/web/docker/build_and_push.sh
@@ -1,9 +1,12 @@
 USAGE="[-t <TAG>]"
+USAGE="[-a <ARG>]"
 
-while getopts t:h opt; do
+while getopts a:t:h opt; do
   case $opt in
+      a) arg=${OPTARG}
+         echo "$OPTARG";;
       t) tag=${OPTARG}
-         ;;
+         echo "$OPTARG";;
       *) usage
          ;;
   esac
@@ -11,6 +14,7 @@ done
 shift $((OPTIND - 1))
 
 IMAGE_NAME_AND_TAG=niku100g/text-analysis-web:${tag:-"latest"}
+ARG=${arg:-".env.development"}
 
-DOCKER_BUILDKIT=1 docker build -t $IMAGE_NAME_AND_TAG -f ./Dockerfile ../../../web
+DOCKER_BUILDKIT=1 docker build -f ./Dockerfile ../../../web --build-arg ENV_FILE_NAME=$ARG -t $IMAGE_NAME_AND_TAG
 docker push $IMAGE_NAME_AND_TAG
